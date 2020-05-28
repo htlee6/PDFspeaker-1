047199ff93177c7bc04c4f7e0e0084c2edb72e4f
htlee <w29593617@gmail.com>
2020-05-28 21:05:06 +0800

    Initial Commit

---
+++ PDFspeaker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PDFspeaker
+//
+//  Created by 李昊天 on 2020/5/28.
+//  Copyright © 2020 HauTen Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
